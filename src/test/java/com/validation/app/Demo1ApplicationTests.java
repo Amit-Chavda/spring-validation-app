7b9c89ba597747672eb9a1406657178638c4aa2f
Amit Chavda <amitchavda9879@gmail.com>
2022-08-02 14:40:55 +0530

    initial commit

---
+++ src/test/java/com/validation/app/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validation.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
